cacb430d6e3d632a0113286fb0aa0901edf26448
Trey Smith <treysmith@Treys-MacBook-Pro.local>
2018-04-24 11:06:25 -0400

    Line in form set

--- Escape/index.php
+++ Escape/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<title>Lunar Escape </title>
+<title>Lunar Escape</title>
 <meta charset="UTF-8">
 <meta name="keywords" content=""/>
 <link href="css/style.css" rel="stylesheet"/>
@@ -21,7 +21,7 @@ color: white;
 </head>
 
 <body>
-    <div id="fontLine"><h1 id="mainText">Orbital Escape</h1></div>
+    <div id="fontLine" onclick="mainLine()"><div id="mainText">Orbital Escape</div>
     <div id="loginForm">
         <h3>Members Login</h3>
         <input type="text" name"userName" id="userName" placeholder="UserName">
@@ -29,5 +29,6 @@ color: white;
         <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
 
    </div>
+<script type="text/javascript" src="js/scripts.js">
 </body>
 </html>
---
+++ Escape/js/scripts.js
@@ -0,0 +1,5 @@
+  function mainLine(){
+
+   alert("test");
+
+  }
