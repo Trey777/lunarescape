be018a260b791011678415dc47f584c7eb7e70a9
Trey Smith <treysmith@Treys-MacBook-Pro.local>
2018-04-30 15:51:22 -0400

    FrontPageWork

--- Escape/index.php
+++ Escape/index.php
@@ -31,16 +31,6 @@ color: white;
 
 </div>
     <div id="fontLine" onclick="mainLine()"><div id="mainText">Orbital Escape</div>
-    <div id="loginForm">
-    <form method="post"  action="memberLogin.php">
-        <h3>Members Login</h3>
-        <input type="text" name"userName" id="userName" placeholder="UserName">
-        <input type="password" name="userPassword" id="userPassword" placeholder="Password">
-        <input type="submit" name="submitButton" value="Log in"/>
-    </form>
-    <h3>If you do not have an account, click here</h3>
-   </div>
-<script type="text/javascript" src="js/scripts.js">
 </script>
 </body>
 </html>
---
+++ Escape/indexfront.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en"/>
+
+<head>
+    <title>OE Procedure</title>
+    <meta charset="UTF-8"/>
+    <meta name="keywords" content=""/>
+    <meta name="title" content=""/>
+    <style>
+        body{
+        background-color:black;
+}
+    .noticeText{
+    color:red;
+    font-style: italic;
+    margin: 0 auto;
+    position: relative;
+}
+#fontPageBox{
+    left: 35vw;
+    top: 10vw;
+    border: 3pt solid crimson;
+    margin: 0 auto;
+    background-color: skyblue;
+    opacity: 0.4;
+    height: 500px;
+    width: 500px;
+    border-radius: 15px;
+    position: absolute;
+}
+#frontFooter{
+   top: 350px;
+}
+    </style>
+</head>
+
+<body>
+    <div id="fontPageBox">
+        <div id="frontHeader">
+        <h4 class="noticeText">***Notice: For Offical use only!!!! Non-restricted use is forbidden ****</h4>
+        </div>
+
+        <div id="frontFooter">
+            <h4 class="noticeText" id="frontFooter">***Notice: For Offical use only!!!! Non-restricted use is forbidden ****</h4>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
