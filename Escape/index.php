4adb34ccbbbcf5ab4b2f004f6146e7f2a7db143b
Trey Smith <treysmith@TreysMac.local>
2018-05-13 21:06:47 -0400

    Correcting LoginProblems

--- Escape/index.php
+++ Escape/index.php
@@ -15,15 +15,19 @@ if(isset($_POST['signupSubmit'])){
 	$firstName = $_POST['firstName'];
 	$firstName = stripslashes($firstName); //security procedure (Probably not needed);
 	$firstName = mysqli_real_escape_string($conn, $firstName); //Security Procedure
+	$firstLen = strlen($firstName);
+	$firstCharFN = substr($firstName, 0, 1);
+	$capFirst = ucfirst($firstCharFN);
 	
-
+	
 	$lastName = $_POST['lastName']; 
 	$lastName = stripslashes($lastName); 
 	$lastName = mysqli_real_escape_string($conn, $lastName); 
+	$lastLen = strlen($lastName);
+	$firstCharLN = substr($lastName, 0, 1);
+	$capLast = ucfirst($firstCharLN);
 
-    $nameArr = array($firstName, $lastName); 	
-	$newName = substr($nameArr, 0, 1);
-	
+ 	$newName = $capLast.$capFirst."-".$firstLen.$lastLen;
 		
 	$userName = $_POST['userName'];
 	$userName = stripslashes($lastName); 
@@ -33,13 +37,12 @@ if(isset($_POST['signupSubmit'])){
 	$userPass = mysqli_real_escape_string($conn, $userPass);
 	$hashedPass = password_hash($userPass, PASSWORD_DEFAULT);
 
-	$query = "INSERT INTO members (firstName, lastName, user, pswd) ";
-	$query .="VALUES ('$firstName', '$lastName', '$userName', '$hashedPass')";
+	$query = "INSERT INTO members (firstName, lastName, user, pswd, newName) ";
+	$query .="VALUES ('$firstName', '$lastName', '$userName', '$hashedPass', '$newName')";
 	mysqli_query($conn, $query);
 
 if(mysqli_affected_rows($conn) == 1){
-	header("Refresh: 4; url=index.php#login", true, 303);
-	$msg = "I think it worked...";
+	//$msg = "I think it worked...";
 }
 else{
 	//$msg = "I dont think it worked...";
@@ -50,13 +53,13 @@ else{
 
 	if(isset($_POST['loginSubmit'])){
 
-	$user = $_POST['userLogin'];
-	$user = stripslashes($user); //security procedure (Probably not needed);
-	$user = mysqli_real_escape_string($conn, $user); //Security procedure
+	$newForm = $_POST['newForm'];
+	$newForm = stripslashes($newForm); //security procedure (Probably not needed);
+	$newForm = mysqli_real_escape_string($conn, $newForm); //Security procedure
 
 	$pass = $_POST['passwordLogin'];
 
-	$query = "SELECT * FROM members WHERE user = '$user'";
+	$query = "SELECT * FROM members WHERE newName = '$newForm'";
     $results =	mysqli_query($conn, $query);
 
 	$row = mysqli_fetch_array($results);
@@ -65,8 +68,6 @@ else{
 
 		if(password_verify($pass, $row['pswd'])){
 
-		
-		$msg = "<span style='color:green; font-weight:bold;'>Initializing Successful Access.</span>";
 		header("Refresh: 5; url=welcomepage.php", true, 303);
 
 		}
@@ -134,10 +135,10 @@ else{
 <div id="login">
 <h1><?php echo $newName ?></h1>
 	<p>	
-	<form method='POST' action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<form method='POST' action="welcomepage.php">
 		<img src="images/logo.png" class="OEpic"/></br></br>
 		<div class='field-wrap'>
-			<input type='text' name='userLogin' id='userLogin' class='required' required autocomplete='off' placeholder="New ID:      (example: RS-37)"/>
+			<input type='text' name='newForm' id='userLogin' class='required' required autocomplete='off' placeholder="New ID:      (example: RS-37)"/>
 		</div>
 
 
@@ -148,7 +149,7 @@ else{
 
 		</br></br>  <!-- had to be done because my height changes to form completely altered outlook for some reason. -->
 
-		<button type='submit' class='button button-block' id='loginSubmit' name='loginSubmit'/>⦕ ⦖</button>
+		<input type='submit' class='button button-block' id='loginSubmit' name='loginSubmit'/>⦕ ⦖</input>
 	</div>
 </div>
 
